c7b387c33ed9e1a8e02b30a47a1d0f1d6503aa40
jasper <jasper.massink01@gmail.com>
2019-07-02 13:24:07 +0200

    multi select component added

---
+++ MultiSelect.APP/src/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultiSelectComponent } from './multi-select/multi-select.component';
+
+@NgModule({
+  declarations: [MultiSelectComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [MultiSelectComponent]
+})
+export class SharedModule { }
